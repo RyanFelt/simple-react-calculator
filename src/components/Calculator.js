e86429c4241eca086181a1d6f3cda02a774690b0
Ryan Feltkamp <ryanwfeltkamp@gmail.com>
2019-07-30 14:00:05 -0400

    . bug and restructure

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -15,72 +15,79 @@ export default class Calculator extends Component {
     };
   }
 
-  onClickItem = props => {
-    if (props.t === 'INPUT') {
-      if (this.state.activeOperator) {
-        let newDisplay = this.state.secondNum + props.v;
-        if (newDisplay === '.') newDisplay = '0.';
-        this.setState({
-          secondNum: newDisplay,
-          display: newDisplay
-        });
-      } else {
-        let newDisplay = this.state.display + props.v;
-        if (newDisplay === '.') newDisplay = '0.';
+  input = value => {
+    if (this.state.activeOperator) {
+      if (value === '.' && this.state.secondNum.includes('.')) return;
+      let newDisplay = this.state.secondNum + value;
+      if (newDisplay === '.') newDisplay = '0.';
+      this.setState({
+        secondNum: newDisplay,
+        display: newDisplay
+      });
+    } else {
+      if (value === '.' && this.state.display.includes('.')) return;
+      let newDisplay = this.state.display + value;
+      if (newDisplay === '.') newDisplay = '0.';
 
-        this.setState({
-          display: newDisplay,
-          firstNum: newDisplay
-        });
-      }
-    } else if (props.t === 'OPERATOR') {
-      if (props.v === 'CLEAR') {
-        this.setState({
-          display: '',
-          firstNum: '',
-          secondNum: '',
-          operator: false,
-          activeOperator: false,
-          activeButton: ''
-        });
-      } else if (props.v === 'INVERSE') {
-        if (!this.state.display.includes('-') && this.state.display !== '')
-          this.setState({
-            display: `-${this.state.display}`
-          });
-        else if (this.state.display !== '')
-          this.setState({
-            display: this.state.display.split('-')[1]
-          });
-      } else if (props.v === 'PERCENTAGE') {
-        const percent = this.state.display / 100;
-        this.setState({ display: percent.toString() });
-      } else if (props.v === 'EQUAL') {
-        if (!this.state.operator) return;
-        const res = math({
-          operator: this.state.operator,
-          firstNum: this.state.firstNum,
-          secondNum: this.state.secondNum
-        }).toString();
-
-        this.setState({
-          display: res,
-          firstNum: res,
-          activeOperator: true,
-          activeButton: ''
-        });
-      }
-      // +, -, /, *
-      else {
-        this.setState({
-          operator: props.v,
-          activeOperator: true,
-          firstNum: this.state.display,
-          secondNum: '',
-          activeButton: props.v
-        });
-      }
+      this.setState({
+        display: newDisplay,
+        firstNum: newDisplay
+      });
     }
+  };
+
+  operator = props => {
+    if (props.v === 'CLEAR') {
+      this.setState({
+        display: '',
+        firstNum: '',
+        secondNum: '',
+        operator: false,
+        activeOperator: false,
+        activeButton: ''
+      });
+    } else if (props.v === 'INVERSE') {
+      if (!this.state.display.includes('-') && this.state.display !== '')
+        this.setState({
+          display: `-${this.state.display}`
+        });
+      else if (this.state.display !== '')
+        this.setState({
+          display: this.state.display.split('-')[1]
+        });
+    } else if (props.v === 'PERCENTAGE') {
+      const percent = this.state.display / 100;
+      this.setState({ display: percent.toString() });
+    } else if (props.v === 'EQUAL') {
+      if (!this.state.operator) return;
+      const res = math({
+        operator: this.state.operator,
+        firstNum: this.state.firstNum,
+        secondNum: this.state.secondNum
+      }).toString();
+
+      this.setState({
+        display: res,
+        firstNum: res,
+        activeOperator: true,
+        activeButton: ''
+      });
+    }
+    // +, -, /, *
+    else {
+      this.setState({
+        operator: props.v,
+        activeOperator: true,
+        firstNum: this.state.display,
+        secondNum: '',
+        activeButton: props.v
+      });
+    }
+  };
+
+  onClickItem = props => {
+    if (props.t === 'INPUT') this.input(props.v);
+    else if (props.t === 'OPERATOR') this.operator(props);
   };
 
   render() {
